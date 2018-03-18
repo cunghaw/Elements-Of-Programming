896434a506d0f3b247feb1bae912449c061379a2
Ronny <rlim6@gatech.edu>
2018-03-18 12:23:42 -0400

    Fixed invalid solution of 7.8 problem

--- 7.8 Remove duplicates from a sorted list/main.py
+++ 7.8 Remove duplicates from a sorted list/main.py
@@ -1,32 +1,23 @@
 # -*- coding: utf-8 -*-
 """
-Base convertion.
+Remove duplicates from a sorted list.
 @author: Ronny
 """
 
-TABLE_STR_NUM = { '0' : 0,  '1' : 1,  '2' : 2,  '3' : 3, 
-                  '4' : 4,  '5' : 5,  '6' : 6,  '7' : 7, 
-				  '8' : 8,  '9' : 9,  'A' : 10, 'B' : 11,
-				  'C' : 12, 'D' : 13, 'E' : 14, 'F' : 15  }
+def removeDuplicates( sortedList ):
+	last_data = None
+	idx = 0
+	for curr_data in sortedList[:]:
+		if last_data == curr_data:
+			del sortedList[idx]
+		else:
+			last_data = curr_data
+			idx += 1
 
-TABLE_NUM_STR = { k : v for v, k in TABLE_STR_NUM.items() }
-
-def convert ( strNum, base1, base2 ):
-	total = 0
-	for mul, char in enumerate( reversed( strNum ), 0 ):
-		total += ( base1 ** mul ) * TABLE_STR_NUM[char]
-	
-	result = ""
-	while ( total > 0 ):
-		num = int( total / base2 )
-		mod = total % base2
-		result = "%s%s" % ( TABLE_NUM_STR[ mod ], result ) 
-		total = num
-	return result
-	
+	return sortedList
+			
 if __name__ == '__main__':
-	assert( convert( "615", 7, 13 ) == "1A7" )
-	assert( convert( "1A7", 13, 7 ) == "615" )
-	assert( convert( "615", 7, 2  ) == '100110010' )
-	assert( convert( "1A7", 13, 2 ) == '100110010' )
+	assert( removeDuplicates( [ 1, 2, 2, 3 ] ) == [ 1, 2, 3 ] )
+	assert( removeDuplicates( [ 1, 2, 3 ] ) == [ 1, 2, 3 ] )
+	assert( removeDuplicates( [ 1, 1, 1 ] ) == [ 1 ] )
 	print "All unit tests are passed"
